c62a916d864aba57c468c91830e15c8398fce017
Samuel <a_samuel.calderon.gonzalez@iespablopicasso.es>
2021-11-30 12:36:21 +0100

    Método para contar cuántas veces aparece cada número del array

--- src/Metodo.java
+++ src/Metodo.java
@@ -3,10 +3,34 @@ public class Metodo {
 
 	public static void main(String[] args) {
 		int[] array = new int[20];
-
+		int[] aux = new int[10];
+		
+		for (int i = 0; i < aux.length; i++) {
+			aux[i] = 0;
+		}
 		for (int i = 0; i < array.length; i++) {
 			array[i] = (int) (Math.random() * 10 + 1);
 			System.out.println(array[i]);
+			if(array[i] == 1)
+				aux[0]++;
+			else if(array[i] == 2)
+				aux[1]++;
+			else if(array[i] == 3)
+				aux[2]++;
+			else if(array[i] == 4)
+				aux[3]++;
+			else if(array[i] == 5)
+				aux[4]++;
+			else if(array[i] == 6)
+				aux[5]++;
+			else if(array[i] == 7)
+				aux[6]++;
+			else if(array[i] == 8)
+				aux[7]++;
+			else if(array[i] == 9)
+				aux[8]++;
+			else if(array[i] == 10)
+				aux[9]++;
 		}
 
 	}
