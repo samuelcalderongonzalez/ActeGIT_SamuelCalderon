694e35730be07345d7268b0b5ca110cae0e0f676
Samuel <a_samuel.calderon.gonzalez@iespablopicasso.es>
2021-11-30 12:11:15 +0100

    Metodo numeros aleatorios

---
+++ src/Metodo.java
@@ -0,0 +1,13 @@
+
+public class Metodo {
+
+	public static void main(String[] args) {
+		int[] array = new int[20];
+
+		for (int i = 0; i < array.length; i++) {
+			array[i] = (int) (Math.random() * 10 + 1);
+		}
+
+	}
+
+}
